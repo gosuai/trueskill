ba7a6c64dc2401d48f95945eaeab0bf4ef500448
dankin96 <dankin96@gmail.com>
2018-08-06 17:18:41 +0300

    fixup! TRUESKILL: Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import (
+    find_packages,
+    setup)
+
+setup(
+    name='trueskill-gosuai',
+    version='1.0.0',
+    description='Trueskill library with imporvements',
+    url='https://github.com/gosuai/trueskill',
+    license='BSD',
+    packages=['trueskill'],
+)
